cd14871d2db53c7570409245d024f2c2aa3a86b6
katherinenholland <katherinenholland@gmail.com>
2014-05-21 16:59:26 -0400

    updates

---
+++ app.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	$('.button').submit(function () {
+		var photo = $(this).value();
+	
+	$.ajax({
+	dataType: "jsonp",
+	url:"http://api.instagram.com/oembed?url=http://instagr.am/p/BUG/",
+	data: data,
+	success: function(foodPic)
+	});			
+	
+	return false;
+	
+	}) //end of submit
+
+}); //end of ready
+
+function foodPic(photo) {
+			if (photo =='pass') {
+				$('.results').html('<div>' + photo + '</div>');
+			} else {	
+			$('.results').html("<div> We didn't find your food!</div>");
+		
+		}//end foodPic
