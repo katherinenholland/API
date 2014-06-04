d222e60c17bd4f8989f71882570dadee4829a25a
katherinenholland <katherinenholland@gmail.com>
2014-06-04 17:19:55 -0400

    update to url

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
 	var api_token="9301596.7d41089.a7f53828b96f43199f0ee424e08e144d";
 	$.ajax({
 	dataType: "jsonp",
-	url:"https://api.instagram.com/v1/tags/search?q=",
-	access_token: api_token,
+	url:"https://api.instagram.com/v1/tags/search?q="+ photo + "&" + "access_token=" + api_token,
+	data: api_token,
 	success: foodPic(photo)
 	});		
 	
