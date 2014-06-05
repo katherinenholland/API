8b59d10ffe3e40872db12bd898d12f30fb4fc8d2
katherinenholland <katherinenholland@gmail.com>
2014-06-05 14:52:59 -0400

    app.js and html update
    api is working with new success function in app.js
    updated html to change one class from results to pics

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('.button').click(function () {
-	
 	var photo = $('.yum').val();
+	
 
 	var api_token="9301596.7d41089.a7f53828b96f43199f0ee424e08e144d";
 	
@@ -9,24 +9,26 @@ $(document).ready(function() {
 	dataType: "jsonp",
 	url:"https://api.instagram.com/v1/tags/" + photo +"/media/recent/?" + "access_token=" + api_token,
 	data: api_token,
-	success: foodPic
-	});		
+	success: function(data) {
+		for (var i = 0; i < 20; i++) {
+		$(".pics").append("<a target='_blank' href='" + data.data[i].link +
+		"'><img src='" + data.data[i].images.low_resolution.url +"'></img></a>");
+		}
+	  }
+	});//end of ajax call		
 	
-	//return false;
-	
+	//$('.yum').val("");
+
 	}) //end of submit
 
+	newSearch();
+
+}); //end of ready
 
 
-function foodPic(photo) {
-			
-			if (photo =='pass') {
-			alert('success');	
-			$('.results').html('<div>' + photo + '</div>');
-			} else if (photo == 'fail') {	
-			$('.results').html("<div> We didn't find your food!</div>");
-		}
 
-		};//end foodPic
-
-	}); //end of ready
+newSearch = function() {
+	$('.button2').click(function() {
+		location.reload();
+	})
+}; //reloads page to start a new game
